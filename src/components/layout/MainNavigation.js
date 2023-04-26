bc0d69f861d815a4dedbcd3712976c3c00f762c8
Guillermina Lorenzo Martinez <glm1a23@soton.ac.uk>
2023-04-26 17:10:08 +0100

    Added style to MainNavigation

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom";
 
+import MainNavigationStyle from "./MainNavigation.module.css";
+
 function MainNavigation() {
   return (
-    <header>
-      <div>React Meetups</div>
+    <header className={MainNavigationStyle.header}>
+      <div className={MainNavigationStyle.logo}>React Meetups</div>
       <nav>
         <ul>
           <li>
-            <Link to='/'>All Meetups</Link>
+            <Link to="/">All Meetups</Link>
           </li>
           <li>
-            <Link to='/new-meetup'>New Meetup</Link>
+            <Link to="/new-meetup">New Meetup</Link>
           </li>
           <li>
-            <Link to='/favourites'>Favourites</Link>
+            <Link to="/favourites">Favourites</Link>
           </li>
         </ul>
       </nav>
