ff0c9c7a4940ac2b3ff1142c6ce59f1ca1a9870d
Guillermina Lorenzo Martinez <glm1a23@soton.ac.uk>
2023-04-28 14:51:06 +0100

    Updated MeetupItem component

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,17 +1,21 @@
+import MeetupItemStyle from "./MeetupItem.module.css";
+
 function MeetupItem(props) {
   return (
-    <li>
-      <div>
+    <li className={MeetupItemStyle.item}>
+      <div className={MeetupItemStyle.image}>
         <img src={props.image} alt={props.title} />
       </div>
-      <div>
+      <div className={MeetupItemStyle.content}>
         <h3>{props.title}</h3>
         <address>{props.address}</address>
         <p>{props.description}</p>
       </div>
-      <div>
+      <div className={MeetupItemStyle.actions}>
         <button>Favourite</button>
       </div>
     </li>
   );
 }
+
+export default MeetupItem;
